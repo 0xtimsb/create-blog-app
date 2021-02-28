e85b5f473fd04cafaea685e49f93330db2950e5b
Smit Barmase <smitbarmase@outlook.com>
2021-02-28 18:20:34 +0530

    git and install added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import fs from "fs";
 import ncp from "ncp";
 import path from "path";
 import { promisify } from "util";
+import execa from "execa";
+import Listr from "listr";
+import { projectInstall } from "pkg-install";
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
@@ -11,6 +14,16 @@ async function copyTemplateFiles(options) {
   return copy(options.templateDirectory, options.targetDirectory, {
     clobber: false,
   });
+}
+
+async function initGit(options) {
+  const result = await execa("git", ["init"], {
+    cwd: options.targetDirectory,
+  });
+  if (result.failed) {
+    return Promise.reject(new Error("Failed to initialize git"));
+  }
+  return;
 }
 
 export async function createProject(options) {
@@ -41,9 +54,26 @@ export async function createProject(options) {
     process.exit(1);
   }
 
-  console.log("Copying project files...");
-  await copyTemplateFiles(options);
+  const tasks = new Listr([
+    {
+      title: "Copy project files",
+      task: () => copyTemplateFiles(options),
+    },
+    {
+      title: "Initialize git",
+      task: () => initGit(options),
+    },
+    {
+      title: "Install dependencies",
+      task: () =>
+        projectInstall({
+          cwd: options.targetDirectory,
+        }),
+    },
+  ]);
 
-  console.log("%s Blog ready", chalk.green.bold("DONE"));
+  await tasks.run();
+
+  console.log("%s Blog ready to publish!", chalk.green.bold("DONE"));
   return true;
 }
